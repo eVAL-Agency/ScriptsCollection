d8477b71ccbe2fd0edb80f191c2b944b1e0d1fc0
Charlie Powell <cdp1337@veraciousnetwork.com>
2024-12-22 09:00:23 -0500

    Add some runtime debugging checks

--- compile.py
+++ compile.py
@@ -2,7 +2,7 @@ from glob import glob
 from pprint import pprint
 import os
 
-def parse_include(include, scriptlets):
+def parse_include(src_file: str, src_line: int, include: str, scriptlets: list):
 	dependencies = ''
 	output = ''
 
@@ -11,11 +11,14 @@ def parse_include(include, scriptlets):
 		file = os.path.join('scriptlets', include)
 		if os.path.exists(file):
 			output += '# scriptlet: ' + include + '\n'
+			line_number = 0
+
 			with open(file, 'r') as include_f:
 				for include_line in include_f:
+					line_number += 1
 					if include_line.startswith('# scriptlet:'):
 						sub_include = include_line[12:].strip()
-						dependencies += parse_include(sub_include, scriptlets)
+						dependencies += parse_include(file, line_number, sub_include, scriptlets)
 					else:
 						output += include_line
 			if not output.endswith("\n"):
@@ -23,6 +26,8 @@ def parse_include(include, scriptlets):
 			output += '# end-scriptlet: ' + include + '\n\n'
 		else:
 			output += '# ERROR - scriptlet ' + include+ ' not found\n\n'
+			print('ERROR - script %s not found' % include)
+			print('  in file %s at line %d' % (src_file, src_line))
 
 	return dependencies + output
 
@@ -34,13 +39,15 @@ for file in glob('src/**/*.sh', recursive=True):
 		os.makedirs(os.path.dirname(dest_file))
 
 	scriptlets = []
+	line_number = 0
 
 	with open(file, 'r') as f:
 		with open(dest_file, 'w') as dest_f:
 			for line in f:
+				line_number += 1
 				# Check for "# scriptlet:..." replacements
 				if line.startswith('# scriptlet:'):
 					include = line[12:].strip()
-					dest_f.write(parse_include(include, scriptlets))
+					dest_f.write(parse_include(file, line_number, include, scriptlets))
 				else:
 					dest_f.write(line)
