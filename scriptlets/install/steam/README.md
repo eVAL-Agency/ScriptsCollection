1fc77c2071c6654a3e08cded0b75cde0a9dbe050
Charlie Powell <cdp1337@veraciousnetwork.com>
2024-12-22 11:22:40 -0500

    Add Steam auto-accept EULA for non-interactive runs

---
+++ scriptlets/install/steam/README.md
@@ -0,0 +1,3 @@
+# SteamCMD installation scriptlet
+
+Usage of this script requires acceptance of the user agreement for Steam.
--- scriptlets/install/steam/install-steamcmd.sh
+++ scriptlets/install/steam/install-steamcmd.sh
@@ -29,10 +29,20 @@ function install_steamcmd() {
 		# Enable "non-free" repos for Debian (for steamcmd)
 		# https://stackoverflow.com/questions/76688863/apt-add-repository-doesnt-work-on-debian-12
 		add-apt-repository -y -U http://deb.debian.org/debian -c non-free-firmware -c non-free
+		if [ $? -ne 0 ]; then
+			echo "Workaround failed to add non-free repos, trying new method instead"
+			apt-add-repository -y non-free
+		fi
 
 		# Install steam repo
 		curl -s http://repo.steampowered.com/steam/archive/stable/steam.gpg > /usr/share/keyrings/steam.gpg
 		echo "deb [arch=amd64,i386 signed-by=/usr/share/keyrings/steam.gpg] http://repo.steampowered.com/steam/ stable steam" > /etc/apt/sources.list.d/steam.list
+
+		# By using this script, you agree to the Steam license agreement at https://store.steampowered.com/subscriber_agreement/
+		# and the Steam privacy policy at https://store.steampowered.com/privacy_agreement/
+		# Since this is meant to support unattended installs, we will forward your acceptance of their license.
+		echo steam steam/question select "I AGREE" | debconf-set-selections
+		echo steam steam/license note '' | debconf-set-selections
 
 		# Install steam binary and steamcmd
 		apt update
