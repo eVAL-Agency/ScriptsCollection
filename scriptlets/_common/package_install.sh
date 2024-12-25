282a49419164c7d346f0c6d24fbb879a4fe98c33
Charlie Powell <cdp1337@veraciousnetwork.com>
2024-12-25 10:40:36 -0500

    Add package_install scriptlet

---
+++ scriptlets/_common/package_install.sh
@@ -0,0 +1,41 @@
+# scriptlet: _common/os_like.sh
+
+##
+# Install a package with the system's package manager.
+#
+# Uses Redhat's yum, Debian's apt-get, and SuSE's zypper.
+#
+# Usage:
+#
+# ```syntax-shell
+# package_install apache2 php7.0 mariadb-server
+# ```
+#
+# @param $1..$N string
+#        Package, (or packages), to install.  Accepts multiple packages at once.
+#
+function package_install (){
+	echo "Installing $*..."
+
+	TYPE_BSD="$(os_like_bsd)"
+	TYPE_DEBIAN="$(os_like_debian)"
+	TYPE_RHEL="$(os_like_rhel)"
+	TYPE_ARCH="$(os_like_arch)"
+	TYPE_SUSE="$(os_like_suse)"
+
+	if [ "$TYPE_BSD" == 1 ]; then
+		pkg install -y $*
+	elif [ "$TYPE_DEBIAN" == 1 ]; then
+		apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" install -y $*
+	elif [ "$TYPE_RHEL" == 1 ]; then
+		yum install -y $*
+	elif [ "$TYPE_ARCH" == 1 ]; then
+		pacman -Syu --noconfirm $*
+	elif [ "$TYPE_SUSE" == 1 ]; then
+		zypper install -y $*
+	else
+		echo 'Unsupported or unknown OS' >&2
+		echo 'Please report this at https://github.com/cdp1337/ScriptsCollection/issues' >&2
+		exit 1
+	fi
+}
