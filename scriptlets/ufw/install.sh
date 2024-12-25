990d2b7ffdef01eaee9ab9655c343bfa36601e47
Charlie Powell <cdp1337@veraciousnetwork.com>
2024-12-25 10:39:53 -0500

    Add UFW installation scriptlet

---
+++ scriptlets/ufw/install.sh
@@ -0,0 +1,14 @@
+# scriptlet: _common/package_install.sh
+# scriptlet: _common/os_like.sh
+
+##
+# Install UFW
+#
+function install_ufw() {
+	if [ "$(os_like_rhel)" == 1 ]; then
+		# RHEL/CentOS requires EPEL to be installed first
+		package_install epel-release
+	fi
+
+	package_install ufw
+}
