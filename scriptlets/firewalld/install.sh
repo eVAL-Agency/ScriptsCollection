0dc7cd29be92b79a7cfb9f49bc6ed01543428ba4
Charlie Powell <cdp1337@veraciousnetwork.com>
2024-12-29 11:39:07 -0500

    Add user IP to trusted on firewalld installs

--- scriptlets/firewalld/install.sh
+++ scriptlets/firewalld/install.sh
@@ -5,4 +5,11 @@
 #
 function install_firewalld() {
 	package_install firewalld
+
+	# Auto-add the current user's remote IP to the whitelist (anti-lockout rule)
+	local TTY_IP="$(who am i | awk '{print $5}' | sed 's/[()]//g')"
+	if [ -n "$TTY_IP" ]; then
+		# Anti-lockout rule based on first install of firewalld
+		firewall-cmd --zone=trusted --add-source=$TTY_IP --permanent
+	fi
 }
