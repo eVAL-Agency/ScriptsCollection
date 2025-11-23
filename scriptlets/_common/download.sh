f21fde2ffd31515f468a5be427a158190caffae7
Charlie Powell <cdp1337@veraciousnetwork.com>
2025-11-23 15:34:02 -0500

    Update download to use temp locations and only move to the final location on a successful download.

--- scriptlets/_common/download.sh
+++ scriptlets/_common/download.sh
@@ -3,10 +3,14 @@
 #
 # Uses either cURL or wget based on which is available
 #
+# Downloads the file to a temp location initially, then moves it to the final destination
+# upon a successful download to avoid partial files.
+#
 # Returns 0 on success, 1 on failure
 function download() {
 	local SOURCE="$1"
 	local DESTINATION="$2"
+	local TMP=$(mktemp)
 
 	if [ -z "$SOURCE" ] || [ -z "$DESTINATION" ]; then
 		echo "download: Missing required parameters!" >&2
@@ -14,14 +18,16 @@ function download() {
 	fi
 
 	if [ -n "$(which curl)" ]; then
-		if curl -fsL "$SOURCE" -o "$DESTINATION"; then
+		if curl -fsL "$SOURCE" -o "$TMP"; then
+			mv $TMP "$DESTINATION"
 			return 0
 		else
 			echo "download: curl failed to download $SOURCE" >&2
 			return 1
 		fi
 	elif [ -n "$(which wget)" ]; then
-		if wget -q "$SOURCE" -O "$DESTINATION"; then
+		if wget -q "$SOURCE" -O "$TMP"; then
+			mv $TMP "$DESTINATION"
 			return 0
 		else
 			echo "download: wget failed to download $SOURCE" >&2
