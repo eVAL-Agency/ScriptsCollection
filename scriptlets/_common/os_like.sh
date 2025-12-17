ebf89fbf3f3e8200272cc44abd7cd247352be91e
Charlie Powell <cdp1337@veraciousnetwork.com>
2025-12-17 10:12:24 -0500

    Add return codes to os_like to allow using this function in a simple check.

--- scriptlets/_common/os_like.sh
+++ scriptlets/_common/os_like.sh
@@ -2,97 +2,152 @@
 # Check if the OS is "like" a certain type
 #
 # ie: "ubuntu" will be like "debian"
+#
+# Returns 0 if true, 1 if false
+# Prints 1 if true, 0 if false
+#
+# Usage:
+#   if [ "$(os_like_debian)" -eq 1 ]; then ... ; fi
+#   if os_like_debian; then ... ; fi
+#
 function os_like_debian() {
 	if [ -f '/etc/os-release' ]; then
 		ID="$(egrep '^ID=' /etc/os-release | sed 's:ID=::')"
 		LIKE="$(egrep '^ID_LIKE=' /etc/os-release | sed 's:ID_LIKE=::')"
 
-		if [[ "$LIKE" =~ 'debian' ]]; then echo 1; return; fi
-		if [[ "$LIKE" =~ 'ubuntu' ]]; then echo 1; return; fi
-		if [ "$ID" == 'debian' ]; then echo 1; return; fi
-		if [ "$ID" == 'ubuntu' ]; then echo 1; return; fi
+		if [[ "$LIKE" =~ 'debian' ]]; then echo 1; return 0; fi
+		if [[ "$LIKE" =~ 'ubuntu' ]]; then echo 1; return 0; fi
+		if [ "$ID" == 'debian' ]; then echo 1; return 0; fi
+		if [ "$ID" == 'ubuntu' ]; then echo 1; return 0; fi
 	fi
 
 	echo 0
+	return 1
 }
 
 ##
 # Check if the OS is "like" a certain type
 #
 # ie: "ubuntu" will be like "debian"
+#
+# Returns 0 if true, 1 if false
+# Prints 1 if true, 0 if false
+#
+# Usage:
+#   if [ "$(os_like_ubuntu)" -eq 1 ]; then ... ; fi
+#   if os_like_ubuntu; then ... ; fi
+#
 function os_like_ubuntu() {
 	if [ -f '/etc/os-release' ]; then
 		ID="$(egrep '^ID=' /etc/os-release | sed 's:ID=::')"
 		LIKE="$(egrep '^ID_LIKE=' /etc/os-release | sed 's:ID_LIKE=::')"
 
-		if [[ "$LIKE" =~ 'ubuntu' ]]; then echo 1; return; fi
-		if [ "$ID" == 'ubuntu' ]; then echo 1; return; fi
+		if [[ "$LIKE" =~ 'ubuntu' ]]; then echo 1; return 0; fi
+		if [ "$ID" == 'ubuntu' ]; then echo 1; return 0; fi
 	fi
 
 	echo 0
+	return 1
 }
 
 ##
 # Check if the OS is "like" a certain type
 #
 # ie: "ubuntu" will be like "debian"
+#
+# Returns 0 if true, 1 if false
+# Prints 1 if true, 0 if false
+#
+# Usage:
+#   if [ "$(os_like_rhel)" -eq 1 ]; then ... ; fi
+#   if os_like_rhel; then ... ; fi
+#
 function os_like_rhel() {
 	if [ -f '/etc/os-release' ]; then
 		ID="$(egrep '^ID=' /etc/os-release | sed 's:ID=::')"
 		LIKE="$(egrep '^ID_LIKE=' /etc/os-release | sed 's:ID_LIKE=::')"
 
-		if [[ "$LIKE" =~ 'rhel' ]]; then echo 1; return; fi
-		if [[ "$LIKE" =~ 'fedora' ]]; then echo 1; return; fi
-		if [[ "$LIKE" =~ 'centos' ]]; then echo 1; return; fi
-		if [ "$ID" == 'rhel' ]; then echo 1; return; fi
-		if [ "$ID" == 'fedora' ]; then echo 1; return; fi
-		if [ "$ID" == 'centos' ]; then echo 1; return; fi
+		if [[ "$LIKE" =~ 'rhel' ]]; then echo 1; return 0; fi
+		if [[ "$LIKE" =~ 'fedora' ]]; then echo 1; return 0; fi
+		if [[ "$LIKE" =~ 'centos' ]]; then echo 1; return 0; fi
+		if [ "$ID" == 'rhel' ]; then echo 1; return 0; fi
+		if [ "$ID" == 'fedora' ]; then echo 1; return 0; fi
+		if [ "$ID" == 'centos' ]; then echo 1; return 0; fi
 	fi
 
 	echo 0
+	return 1
 }
 
 ##
 # Check if the OS is "like" a certain type
 #
 # ie: "ubuntu" will be like "debian"
+#
+# Returns 0 if true, 1 if false
+# Prints 1 if true, 0 if false
+#
+# Usage:
+#   if [ "$(os_like_suse)" -eq 1 ]; then ... ; fi
+#   if os_like_suse; then ... ; fi
+#
 function os_like_suse() {
 	if [ -f '/etc/os-release' ]; then
 		ID="$(egrep '^ID=' /etc/os-release | sed 's:ID=::')"
 		LIKE="$(egrep '^ID_LIKE=' /etc/os-release | sed 's:ID_LIKE=::')"
 
-		if [[ "$LIKE" =~ 'suse' ]]; then echo 1; return; fi
-		if [ "$ID" == 'suse' ]; then echo 1; return; fi
+		if [[ "$LIKE" =~ 'suse' ]]; then echo 1; return 0; fi
+		if [ "$ID" == 'suse' ]; then echo 1; return 0; fi
 	fi
 
 	echo 0
+	return 1
 }
 
 ##
 # Check if the OS is "like" a certain type
 #
 # ie: "ubuntu" will be like "debian"
+#
+# Returns 0 if true, 1 if false
+# Prints 1 if true, 0 if false
+#
+# Usage:
+#   if [ "$(os_like_arch)" -eq 1 ]; then ... ; fi
+#   if os_like_arch; then ... ; fi
+#
 function os_like_arch() {
 	if [ -f '/etc/os-release' ]; then
 		ID="$(egrep '^ID=' /etc/os-release | sed 's:ID=::')"
 		LIKE="$(egrep '^ID_LIKE=' /etc/os-release | sed 's:ID_LIKE=::')"
 
-		if [[ "$LIKE" =~ 'arch' ]]; then echo 1; return; fi
-		if [ "$ID" == 'arch' ]; then echo 1; return; fi
+		if [[ "$LIKE" =~ 'arch' ]]; then echo 1; return 0; fi
+		if [ "$ID" == 'arch' ]; then echo 1; return 0; fi
 	fi
 
 	echo 0
+	return 1
 }
 
 ##
 # Check if the OS is "like" a certain type
 #
 # ie: "ubuntu" will be like "debian"
+#
+# Returns 0 if true, 1 if false
+# Prints 1 if true, 0 if false
+#
+# Usage:
+#   if [ "$(os_like_bsd)" -eq 1 ]; then ... ; fi
+#   if os_like_bsd; then ... ; fi
+#
 function os_like_bsd() {
 	if [ "$(uname -s)" == 'FreeBSD' ]; then
 		echo 1
+		return 0
 	else
 		echo 0
+		return 1
 	fi
 }
 
@@ -100,10 +155,20 @@ function os_like_bsd() {
 # Check if the OS is "like" a certain type
 #
 # ie: "ubuntu" will be like "debian"
+#
+# Returns 0 if true, 1 if false
+# Prints 1 if true, 0 if false
+#
+# Usage:
+#   if [ "$(os_like_macos)" -eq 1 ]; then ... ; fi
+#   if os_like_macos; then ... ; fi
+#
 function os_like_macos() {
 	if [ "$(uname -s)" == 'Darwin' ]; then
 		echo 1
+		return 0
 	else
 		echo 0
+		return 1
 	fi
 }
