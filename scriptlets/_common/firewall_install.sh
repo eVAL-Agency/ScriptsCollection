43b0dcb4965e3f81003ede660aa012c16f609ff7
Charlie Powell <cdp1337@bitsnbytes.dev>
2026-09-13 02:52:38 -0400

    Auto install ipset during firewall installation

--- scriptlets/_common/firewall_install.sh
+++ scriptlets/_common/firewall_install.sh
@@ -2,6 +2,8 @@
 # scriptlet:_common/os_like.sh
 # scriptlet:ufw/install.sh
 # scriptlet:firewalld/install.sh
+# scriptlet:_common/package_install.sh
+# scriptlet:_common/cmd_exists.sh
 
 ##
 # Install the system default firewall based on the OS type
@@ -11,23 +13,28 @@
 # For SUSE, this installs firewalld
 # For other OS types, this defaults to installing UFW
 #
+# CHANGELOG
+#  2026.09.12 - Auto-install ipset along with firewall for working with sets of ips
+#  2026.07.08 - Add support for Arch
+#  2026.03.16 - Initial port from firewall-specific scripts
+#
 function firewall_install() {
-	local FIREWALL
+	if [ "$(get_available_firewall)" == "none" ]; then
+		# No firewall installed yet, install the distro default one
 
-	FIREWALL=$(get_available_firewall)
-	if [ "$FIREWALL" != "none" ]; then
-		return
+		if os_like_debian -q; then
+			install_ufw
+		elif os_like_rhel -q; then
+			install_firewalld
+		elif os_like_suse -q; then
+			install_firewalld
+		elif os_like_arch -q; then
+			install_firewalld
+		else
+			install_ufw
+		fi
 	fi
 
-	if os_like_debian -q; then
-		install_ufw
-	elif os_like_rhel -q; then
-		install_firewalld
-	elif os_like_suse -q; then
-		install_firewalld
-	elif os_like_arch -q; then
-		install_firewalld
-	else
-		install_ufw
-	fi
+	# Ensure ipset is installed; useful for blocklists
+	package_install_if ipset
 }
